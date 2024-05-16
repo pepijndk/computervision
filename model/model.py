a49ae8b46c3f910254a402f183230b696b73f249
mlieftinck <martijn.lieftinck@hotmail.nl>
2024-05-16 11:40:12 +0200

    SRGAN discriminator model

--- model/model.py
+++ model/model.py
@@ -62,6 +62,94 @@ class Generator(BaseModel):
         return x
 
 
+class Discriminator(BaseModel):
+    def __init__(self, dim=64):
+        super().__init__()
+        self.conv1 = nn.Conv2d(
+            out_channels=dim, kernel_size=(4, 4), stride=(2, 2), act=tlx.LeakyReLU, padding='SAME', W_init=W_init,
+            data_format='channels_first')
+        self.conv2 = nn.Conv2d(
+            out_channels=dim * 2, kernel_size=(4, 4), stride=(2, 2), act=None, padding='SAME', W_init=W_init,
+            data_format='channels_first', b_init=None)
+        self.bn1 = nn.BatchNorm2d(num_features=dim * 2, act=tlx.LeakyReLU, gamma_init=G_init,
+                                  data_format='channels_first')
+        self.conv3 = nn.Conv2d(
+            out_channels=dim * 4, kernel_size=(4, 4), stride=(2, 2), act=None, padding='SAME', W_init=W_init,
+            data_format='channels_first', b_init=None)
+        self.bn2 = nn.BatchNorm2d(num_features=dim * 4, act=tlx.LeakyReLU, gamma_init=G_init,
+                                  data_format='channels_first')
+        self.conv4 = nn.Conv2d(
+            out_channels=dim * 8, kernel_size=(4, 4), stride=(2, 2), act=None, padding='SAME', W_init=W_init,
+            data_format='channels_first', b_init=None)
+        self.bn3 = nn.BatchNorm2d(num_features=dim * 8, act=tlx.LeakyReLU, gamma_init=G_init,
+                                  data_format='channels_first')
+        self.conv5 = nn.Conv2d(
+            out_channels=dim * 16, kernel_size=(4, 4), stride=(2, 2), act=None, padding='SAME', W_init=W_init,
+            data_format='channels_first', b_init=None)
+        self.bn4 = nn.BatchNorm2d(num_features=dim * 16, act=tlx.LeakyReLU, gamma_init=G_init,
+                                  data_format='channels_first')
+        self.conv6 = nn.Conv2d(
+            out_channels=dim * 32, kernel_size=(4, 4), stride=(2, 2), act=None, padding='SAME', W_init=W_init,
+            data_format='channels_first', b_init=None)
+        self.bn5 = nn.BatchNorm2d(num_features=dim * 32, act=tlx.LeakyReLU, gamma_init=G_init,
+                                  data_format='channels_first')
+        self.conv7 = nn.Conv2d(
+            out_channels=dim * 16, kernel_size=(1, 1), stride=(1, 1), act=None, padding='SAME', W_init=W_init,
+            data_format='channels_first', b_init=None)
+        self.bn6 = nn.BatchNorm2d(num_features=dim * 16, act=tlx.LeakyReLU, gamma_init=G_init,
+                                  data_format='channels_first')
+        self.conv8 = nn.Conv2d(
+            out_channels=dim * 8, kernel_size=(1, 1), stride=(1, 1), act=None, padding='SAME', W_init=W_init,
+            data_format='channels_first', b_init=None)
+        self.bn7 = nn.BatchNorm2d(num_features=dim * 8, act=None, gamma_init=G_init, data_format='channels_first')
+        self.conv9 = nn.Conv2d(
+            out_channels=dim * 2, kernel_size=(1, 1), stride=(1, 1), act=None, padding='SAME', W_init=W_init,
+            data_format='channels_first', b_init=None)
+        self.bn8 = nn.BatchNorm2d(num_features=dim * 2, act=tlx.LeakyReLU, gamma_init=G_init,
+                                  data_format='channels_first')
+        self.conv10 = nn.Conv2d(
+            out_channels=dim * 2, kernel_size=(3, 3), stride=(1, 1), act=None, padding='SAME', W_init=W_init,
+            data_format='channels_first', b_init=None)
+        self.bn9 = nn.BatchNorm2d(num_features=dim * 2, act=tlx.LeakyReLU, gamma_init=G_init,
+                                  data_format='channels_first')
+        self.conv11 = nn.Conv2d(
+            out_channels=dim * 8, kernel_size=(3, 3), stride=(1, 1), act=None, padding='SAME', W_init=W_init,
+            data_format='channels_first', b_init=None)
+        self.bn10 = nn.BatchNorm2d(num_features=dim * 8, gamma_init=G_init, data_format='channels_first')
+        self.add = nn.Elementwise(combine_fn=tlx.add, act=tlx.LeakyReLU)
+        self.flat = nn.Flatten()
+        self.dense = nn.Linear(out_features=1, W_init=W_init)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.bn1(x)
+        x = self.conv3(x)
+        x = self.bn2(x)
+        x = self.conv4(x)
+        x = self.bn3(x)
+        x = self.conv5(x)
+        x = self.bn4(x)
+        x = self.conv6(x)
+        x = self.bn5(x)
+        x = self.conv7(x)
+        x = self.bn6(x)
+        x = self.conv8(x)
+        x = self.bn7(x)
+        temp = x
+        x = self.conv9(x)
+        x = self.bn8(x)
+        x = self.conv10(x)
+        x = self.bn9(x)
+        x = self.conv11(x)
+        x = self.bn10(x)
+        x = self.add([temp, x])
+        x = self.flat(x)
+        x = self.dense(x)
+
+        return x
+
+
 def make_blocks(n):
     blocks = [ResidualBlock() for _ in n]
     blocks = nn.Sequential(blocks)
